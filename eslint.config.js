9b4dbcb92c82cc250874d90340b794d6a83b1588
Vincent Mukuna <mukunavincent28@gmail.com>
2026-06-24 15:31:27 +0300

    chore: add import sorting and file naming lint rules
    Install prettier-plugin-sort-imports and eslint-plugin-check-file. Add
    editorconfig and tighten VS Code settings so Prettier uses the project
    config without fighting ESLint on save.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,43 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const checkFile = require("eslint-plugin-check-file");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*', '.expo/*', 'node_modules/*', 'ios/*', 'android/*'],
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
+  {
+    files: ["src/app/**/_layout.tsx", "src/app/**/\\[*\\].tsx"],
+    rules: {
+      "check-file/filename-naming-convention": "off",
+    },
+  },
+  {
+    ignores: ["dist/*", ".expo/*", "node_modules/*", "ios/*", "android/*"],
   },
 ]);
